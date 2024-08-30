b556495c933ff611ee8f8d5e5b74fa974744aaba
Andre Mattos <carlosandrematte@gmail.com>
2024-08-30 11:45:17 -0300

    Reorganization

--- scripts/experiments/train_rf.py
+++
@@ -1,9 +0,0 @@
-from damseg_ml.train_rf import RandomForestModel
-
-labels = [0, 1]
-model = RandomForestModel(n_estimators=100, max_features="sqrt", seed=23)
-model.load_train_data("data/sample/dataset_binary.csv")
-model.load_test_data("data/sample/dataset_binary.csv", labels)
-model.fit_model()
-model.test_model()
-print(model.metrics)
