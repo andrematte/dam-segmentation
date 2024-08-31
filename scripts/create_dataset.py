355b56c3f75d5bd0d81bff0de500572548c20b37
Andre Mattos <carlosandrematte@gmail.com>
2024-08-31 17:19:52 -0300

    Multiclass datasets

--- scripts/create_dataset.py
+++ scripts/create_dataset.py
@@ -4,17 +4,32 @@ from dam_segmentation.feature_extraction import create_dataset
 
 # 1 - Create binary dataset
 # Create training dataset
+# train_images = "data/train/images/"
+# train_labels = "data/train/labels_binary/"
+# train_output = "data/train/binary_training.parquet"
+# dataset = create_dataset(train_images, train_labels)
+# dataset.to_parquet(train_output)
+
+
+# # Create test dataset
+# test_images = "data/test/images/"
+# test_labels = "data/test/labels_binary/"
+# test_output = "data/test/binary_test.parquet"
+# dataset = create_dataset(test_images, test_labels)
+# dataset.to_parquet(test_output)
+
+# 2 - Create multiclass dataset
+# Create training dataset
 train_images = "data/train/images/"
-train_labels = "data/train/labels_binary/"
-train_output = "data/train/binary_training.csv"
+train_labels = "data/train/labels_multiclass/"
+train_output = "data/train/multiclass_training.parquet"
 dataset = create_dataset(train_images, train_labels)
-dataset.to_csv(train_output, index=False)
+dataset.to_parquet(train_output)
 
 
 # Create test dataset
 test_images = "data/test/images/"
-test_labels = "data/test/labels_binary/"
-test_output = "data/test/binary_test.csv"
+test_labels = "data/test/labels_multiclass/"
+test_output = "data/test/multiclass_test.parquet"
 dataset = create_dataset(test_images, test_labels)
-dataset.to_csv(test_output, index=False)
-dataset.to_csv(test_output, index=False)
+dataset.to_parquet(test_output)
