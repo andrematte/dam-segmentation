ab0c55d561bbfcb80a08cf58dc70f6a756d1bd87
Andre Mattos <carlosandrematte@gmail.com>
2025-03-17 12:52:29 -0700

    Fixed data directory

--- scripts/create_tabular_dataset.py
+++ scripts/create_tabular_dataset.py
@@ -2,64 +2,64 @@ from dam_segmentation.feature_extraction import create_dataset
 
 # ----------------------- Create Binary Tabular Dataset ---------------------- #
 
-train_images = "../data/train/images/"
-train_labels = "../data/train/mask_binary"
+train_images = "../data/dam-segmentation/train/images/"
+train_labels = "../data/dam-segmentation/train/mask_binary"
 
 train = create_dataset(train_images, train_labels)
 
 train.to_parquet(
-    "../data/train_data_binary.parquet",
+    "../data/dam-segmentation/train_data_binary.parquet",
     index=False,
 )
 
-val_images = "../data/val/images/"
-val_labels = "../data/val/mask_binary/"
+val_images = "../data/dam-segmentation/val/images/"
+val_labels = "../data/dam-segmentation/val/mask_binary/"
 
 val = create_dataset(val_images, val_labels)
 
 val.to_parquet(
-    "../data/val_data_binary.parquet",
+    "../data/dam-segmentation/val_data_binary.parquet",
     index=False,
 )
 
-test_images = "../data/test/images/"
-test_labels = "../data/test/mask_binary"
+test_images = "../data/dam-segmentation/test/images/"
+test_labels = "../data/dam-segmentation/test/mask_binary"
 
 test = create_dataset(test_images, test_labels)
 
 test.to_parquet(
-    "../data/test_data_binary.parquet",
+    "../data/dam-segmentation/test_data_binary.parquet",
     index=False,
 )
 
 # -------------------- Create Multi-Class Tabular Dataset -------------------- #
 
-train_images = "../data/train/images/"
-train_labels = "../data/train/mask_multiclass"
+train_images = "../data/dam-segmentation/train/images/"
+train_labels = "../data/dam-segmentation/train/mask_multiclass"
 
 train = create_dataset(train_images, train_labels)
 
 train.to_parquet(
-    "../data/train_data_multiclass.parquet",
+    "../data/dam-segmentation/train_data_multiclass.parquet",
     index=False,
 )
 
-val_images = "../data/val/images/"
-val_labels = "../data/val/mask_multiclass/"
+val_images = "../data/dam-segmentation/val/images/"
+val_labels = "../data/dam-segmentation/val/mask_multiclass/"
 
 val = create_dataset(val_images, val_labels)
 
 val.to_parquet(
-    "../data/val_data_multiclass.parquet",
+    "../data/dam-segmentation/val_data_multiclass.parquet",
     index=False,
 )
 
-test_images = "../data/test/images/"
-test_labels = "../data/test/mask_multiclass"
+test_images = "../data/dam-segmentation/test/images/"
+test_labels = "../data/dam-segmentation/test/mask_multiclass"
 
 test = create_dataset(test_images, test_labels)
 
 test.to_parquet(
-    "../data/test_data_multiclass.parquet",
+    "../data/dam-segmentation/test_data_multiclass.parquet",
     index=False,
 )
